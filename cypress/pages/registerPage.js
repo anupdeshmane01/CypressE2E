3e878192a1732ce4114d9d1c2ccae73909d1c4f9
unknown <anupdeshmane01@gmail.com>
2024-08-24 17:09:22 +0530

    Cypress E2E testing

---
+++ cypress/pages/registerPage.js
@@ -0,0 +1,57 @@
+export class registerPage{
+    
+
+webLocators={
+
+    firstName: '#input-firstname',
+    lastName: '#input-lastname',
+    email: '#input-email',
+    telephone: '#input-telephone',
+    password:'#input-password',
+    confirmPassword: '#input-confirm',
+    policyCheckbox: 'input[type="checkbox"]',
+    continue: 'input[type="submit"]'
+
+}
+openUrl(){
+    cy.visit(Cypress.env('URL'))
+}
+
+enterFirstName(Fname){
+
+    cy.get(this.webLocators.firstName).type(Fname)
+}
+enterLastName(Lname){
+
+    cy.get(this.webLocators.lastName).type(Lname)
+}
+enterEmail(email){
+
+    cy.get(this.webLocators.email).type(email)
+}
+enterTelephone(telnum){
+
+    cy.get(this.webLocators.telephone).type(telnum)
+}
+enterPassword(password){
+
+    cy.get(this.webLocators.password).type(password)
+    cy.get(this.webLocators.confirmPassword).type(password)
+}
+enterPassword(password){
+
+    cy.get(this.webLocators.password).type(password)
+    cy.get(this.webLocators.confirmPassword).type(password)
+}
+checkBox(){
+
+    cy.get(this.webLocators.policyCheckbox).check()
+}
+
+clickOnContinue(){
+    cy.get(this.webLocators.continue).click()
+}
+
+
+
+}
