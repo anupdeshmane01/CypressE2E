b23e301fc95d41421df67d58b2719d859602cb30
unknown <anupdeshmane01@gmail.com>
2024-08-26 11:56:30 +0530

    New File update

--- cypress/e2e/tests/loginTest.cy.js
+++ cypress/e2e/tests/loginTest.cy.js
@@ -4,7 +4,25 @@ import registerData from "../../fixtures/registerData.json";
 const Login = new login();
 
 describe("Login Test", () => {
-  it("Login Success", () => {
+  it("Login Success 1", () => {
+    Login.loginURL();
+    Login.loginEmail(registerData.login.username);
+    Login.loginPassword(registerData.login.password1);
+    Login.loginBtn();
+  });
+  it("Login Success 2", () => {
+    Login.loginURL();
+    Login.loginEmail(registerData.login.username);
+    Login.loginPassword(registerData.login.password1);
+    Login.loginBtn();
+  });
+  it("Login Success 3", () => {
+    Login.loginURL();
+    Login.loginEmail(registerData.login.username);
+    Login.loginPassword(registerData.login.password1);
+    Login.loginBtn();
+  });
+  it("Login Success 4", () => {
     Login.loginURL();
     Login.loginEmail(registerData.login.username);
     Login.loginPassword(registerData.login.password1);
