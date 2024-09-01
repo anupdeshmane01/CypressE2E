6cfe7b47160791b8d312864c663503bc60c0bd49
unknown <anupdeshmane01@gmail.com>
2024-09-01 17:13:19 +0530

    anup commit

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
+    screenshotOnRunFailure: true,
+    video:true,
     specPattern:"cypress/e2e/**/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
@@ -15,7 +17,7 @@ module.exports = defineConfig({
     URL: 'https://naveenautomationlabs.com/opencart/index.php?route=account/register',
     uname: "Anup"
   },
-  screenshotOnRunFailure: true,
+ 
 
   
 });
